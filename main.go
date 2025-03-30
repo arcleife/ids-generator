b98e83751b756e71bc0ecf9494b71e588451240d
Muhamad Rizky Yanuar <mryanuar21@gmail.com>
2025-03-30 12:47:39 +0900

    added and refactored main router

--- main.go
+++ main.go
@@ -2,65 +2,39 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
+	uuid "github.com/arcleife/ids-generator/internal/uuid"
 	"github.com/gin-gonic/gin"
 )
-
-var db = make(map[string]string)
 
 func setupRouter() *gin.Engine {
 	// Disable Console Color
 	// gin.DisableConsoleColor()
 	r := gin.Default()
 
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
+	// Get uuid
+	r.GET("/uuid", func(c *gin.Context) {
+		delimiter := c.DefaultQuery("delimiter", "-")
+
+		response := uuid.New().All(delimiter)
+		c.String(http.StatusOK, response)
 	})
 
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
+	// Get uuids
+	r.GET("/uuid/bulk", func(c *gin.Context) {
+		n, err := strconv.Atoi(c.Query("n"))
+		delimiter := c.DefaultQuery("delimiter", "-")
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"value":  nil,
+				"status": "n must be integer"})
 		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
-
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
-
-	/* example curl for /admin with basicauth header
-	   Zm9vOmJhcg== is base64("foo:bar")
-
-		curl -X POST \
-	  	http://localhost:8080/admin \
-	  	-H 'authorization: Basic Zm9vOmJhcg==' \
-	  	-H 'content-type: application/json' \
-	  	-d '{"value":"bar"}'
-	*/
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
+			response := uuid.Bulk(n, delimiter)
+			c.JSON(http.StatusOK, gin.H{
+				"value":  response,
+				"status": "OK"})
 		}
 	})
 
