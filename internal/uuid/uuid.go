f492febd3a6fc274a9909fdfe32db9423f83012d
Muhamad Rizky Yanuar <mryanuar21@gmail.com>
2025-03-30 11:47:19 +0900

    refactored uuid module to struct for cleaner code

--- internal/uuid/uuid.go
+++ internal/uuid/uuid.go
@@ -1,26 +1,40 @@
 package internal
 
 import (
-	"log"
 	"strings"
 
 	"github.com/google/uuid"
 )
 
-func Generate() map[string]string {
-	uuid_v7, err := uuid.NewV7()
-	if err != nil {
-		log.Fatal("ERROR: Could not generate UUID")
-	}
+type StructuredUUID struct {
+	all      string
+	timehigh string
+	timemid  string
+	version  string
+	variant  string
+	random   string
+}
 
-	generated_uuid := strings.Split(uuid_v7.String(), "-")
-	result := map[string]string{
-		"timehigh": generated_uuid[0],
-		"timemid":  generated_uuid[1],
-		"version":  generated_uuid[2],
-		"variant":  generated_uuid[3],
-		"random":   generated_uuid[4],
-	}
+func New(uuid uuid.UUID) StructuredUUID {
+	all := uuid.String()
+	generated_uuid := strings.Split(all, "-")
 
-	return result
+	return StructuredUUID{
+		all:      all,
+		timehigh: generated_uuid[0],
+		timemid:  generated_uuid[1],
+		version:  generated_uuid[2],
+		variant:  generated_uuid[3],
+		random:   generated_uuid[4],
+	}
+}
+
+func (u StructuredUUID) ToMap() map[string]string {
+	return map[string]string{
+		"timehigh": u.timehigh,
+		"timemid":  u.timemid,
+		"version":  u.version,
+		"variant":  u.variant,
+		"random":   u.random,
+	}
 }
