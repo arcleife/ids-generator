d6ff375069abb6dc5f50d442566f4a99bbfb5fc6
Muhamad Rizky Yanuar <mryanuar21@gmail.com>
2025-04-09 12:32:08 +0900

    modified base config to enable serving on nginx

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: "/ids-gen",
+    plugins: [react()],
+    preview: {
+        port: 4001,
+        strictPort: true,
+    },
+    server: {
+        port: 4001,
+        strictPort: true,
+        host: true,
+        origin: "http://0.0.0.0:4001",
+    },
+});
